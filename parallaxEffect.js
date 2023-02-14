9a0569bd8bf40108701b0a260ce5f5a3a590faf6
rosematcha <120700018+rosematcha@users.noreply.github.com>
2023-02-13 22:07:57 -0600

    Fix bug with background

--- parallaxEffect.js
+++ parallaxEffect.js
@@ -5,20 +5,20 @@
     // Add event listener
     document.addEventListener("mousemove", parallax);
     const elem = document.querySelector("#parallax");
-    const mediaQuery = window.matchMedia('(max-width: 700px)')
+    const mediaQuery = window.matchMedia('(max-width: 1279px)')
     // Magic happens here
     function parallax(e) {
         let _w = window.innerWidth/2;
         let _h = window.innerHeight/2;
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
-        let _depthBackground = `${50 - (_mouseX - _w) * 0.025}% ${50 - (_mouseY - _h) * 0.025}%`;
+        let _depthBackground = `${50 - (_mouseX - _w) * 0.005}% ${50 - (_mouseY - _h) * 0.01}%`;
         let _depthChupi
-        if (mediaQuery.matches) {
-            _depthChupi = `${43 - (_mouseX - _w) * 0.01}% ${-20 - (_mouseY - _h) * 0.01}%`;
+        if (mediaQuery.matches) { // Mobile
+            _depthChupi = `${43 - (_mouseX - _w) * 0.015}% ${100 - (_mouseY - _h) * 0.01}%`;
         }
-        if (!mediaQuery.matches) {
-            _depthChupi = `${43 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        if (!mediaQuery.matches) { // Desktop
+            _depthChupi = `${43 - (_mouseX - _w) * 0.015}% ${100 - (_mouseY - _h) * 0.01}%`;
         }
         let x = `${_depthChupi},  ${_depthBackground}`;
         console.log(x);
