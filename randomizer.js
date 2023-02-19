8bfc7de035378c96ef59b43c1a037f148f0dd177
rosematcha <120700018+rosematcha@users.noreply.github.com>
2023-02-19 09:53:56 -0600

    Add character design portfolio

--- randomizer.js
+++ randomizer.js
@@ -1,3 +1,9 @@
-$(".modboxMama").html($(".modboxMama .modbox").sort(function(){
-    return Math.random()-0.5;
-}));
+function randomizeImages() {
+    const container = document.getElementById('portfolio');
+    for (let i = container.children.length; i >= 0; i--) {
+      container.appendChild(container.children[Math.random() * i | 0]);
+    }
+  }
+
+  
+document.addEventListener('DOMContentLoaded', randomizeImages);
