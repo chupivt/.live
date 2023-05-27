463939b0cc7001dd64d9c111a8f03e574928fccb
rosematcha <120700018+rosematcha@users.noreply.github.com>
2023-05-27 09:45:45 -0500

    im like john carmack but fruity

--- parallaxEffect.js
+++ parallaxEffect.js
@@ -12,6 +12,7 @@
         let _h = window.innerHeight/2;
         let _mouseX = e.clientX;
         let _mouseY = e.clientY;
+        let _depthEyes = `${50 - (_mouseX - _w) * 0.005}% ${50 - (_mouseY - _h) * 0.01}%`;
         let _depthBackground = `${50 - (_mouseX - _w) * 0.005}% ${50 - (_mouseY - _h) * 0.01}%`;
         let _depthChupi
         if (mediaQuery.matches) { // Mobile
@@ -20,7 +21,8 @@
         if (!mediaQuery.matches) { // Desktop
             _depthChupi = `${60 - (_mouseX - _w) * 0.015}% ${100 - (_mouseY - _h) * 0.01}%`;
         }
-        let x = `${_depthChupi},  ${_depthBackground}`;
+        
+        let x = `${_depthChupi},  ${_depthBackground}, ${_depthBackground}`;
         console.log(x);
         elem.style.backgroundPosition = x;
     }
